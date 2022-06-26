94c765a40e18a847938ac95e41e6ec892f8bd89d
Matt Hamilton <66541468+Matt-Hamilton-GitHub@users.noreply.github.com>
2022-06-25 19:51:44 -0700

    animation added

--- src/elements/Card.jsx
+++ src/elements/Card.jsx
@@ -1,5 +1,46 @@
-const Card = () => {
-  return <></>;
+import styled from "styled-components";
+
+const Card = ({ item }) => {
+  const { id, name, job, image, text } = item;
+  return (
+    <Wrapper>
+      <div className="title-div">
+        <h1>{name}</h1>
+      </div>
+
+      <div className="img-div">
+        <img src={image} alt={name} />
+      </div>
+
+      <div className="desc-div">
+        <p>{text}</p>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Card;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .title-div {
+    background: #6d8b74;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+
+  .img-div {
+  }
+  img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+  }
+
+  .desc-div {
+    padding: 5px;
+    user-select: none;
+  }
+`;
--- src/elements/ProjectSection.jsx
+++ src/elements/ProjectSection.jsx
@@ -7,23 +7,36 @@ import { GrFormPrevious } from "react-icons/gr";
 
 const ProjectSection = () => {
   const [selectedCard, setSelectedCart] = useState();
+  const [currentId, setCurrentId] = useState(0);
 
+  const swipeItem = (prop) => {
+    if (prop === "inc") {
+      if (currentId < prjsDb.length - 3) setCurrentId(currentId + 1);
+      else setCurrentId(0);
+    } else {
+      if (currentId === 0) setCurrentId(prjsDb.length - 3);
+      else setCurrentId(currentId - 1);
+    }
+  };
   return (
     <Wrapper>
-      <div className="svg-icon">
+      <div className="svg-icon" onClick={() => swipeItem("dec")}>
         <GrFormPrevious size={70} />
       </div>
       <div className="project-cards-section">
-        {prjsDb.map((item) => {
-          const { id, title } = item;
+        {prjsDb.slice(currentId, currentId + 3).map((item) => {
+          const { id, name, job, image, text } = item;
           return (
-            <div className={id === 1 ? "card main" : "card"} key={id}>
-              <Card />
+            <div
+              className={id === currentId + 2 ? "card main " : "card"}
+              key={id}
+            >
+              <Card item={item} />
             </div>
           );
         })}
       </div>
-      <div className="svg-icon">
+      <div className="svg-icon" onClick={() => swipeItem("inc")}>
         <MdOutlineNavigateNext size={70} />
       </div>
     </Wrapper>
@@ -39,8 +52,8 @@ const Wrapper = styled.div`
   text-align: center;
   margin: 25px 10px;
   min-height: 300px;
-
   min-width: 100vw;
+  transition: all 3s ease-in;
 
   .project-cards-section {
     display: flex;
@@ -52,6 +65,7 @@ const Wrapper = styled.div`
   }
   .svg-icon {
     display: flex;
+    position: relative;
     flex-direction: column;
     min-height: 100%;
     justify-content: center;
@@ -60,6 +74,8 @@ const Wrapper = styled.div`
     color: white;
   }
   .card {
+    transform: scale(1);
+    transition: all 0.7s ease-in;
     min-width: 200px;
     min-height: 350px;
     background: #293462;
@@ -71,6 +87,7 @@ const Wrapper = styled.div`
 
   .main {
     transform: scale(1.2);
+    transition: all 0.5s ease-in;
     box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
       rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, #354259 0px -2px 0px inset;
   }
